326c53b9210520c86e0f0d073c41dfac04ad6c15
Duy <voanduy1822001@gmail.com>
2024-10-21 16:46:14 +0700

    Update Id

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 const env = {
-    // apiUrl: 'http://localhost:8080',
-    apiUrl: 'http://118.71.15.142:6868',
+      //apiUrl: 'http://localhost:8080',
+   apiUrl: 'http://118.71.62.82:6868',
     // apiUrl: 'http://103.15.222.65:8888',
-    userAvatarUrl: 'http://118.71.15.142:8888/images/'
+    userAvatarUrl: 'http://118.71.62.82:8888/images/'
 };
 
 export default env;
