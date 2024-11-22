74e45df0b55373708e418ebc04d000b3e8952620
Duy <voanduy1822001@gmail.com>
2024-11-22 18:16:40 +0700

    Update Id

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 const env = {
       //apiUrl: 'http://localhost:8080',
-   apiUrl: 'http://171.249.156.192:6868',
+   apiUrl: 'http://171.249.149.131:6868',
     // apiUrl: 'http://103.15.222.65:8888',
     userAvatarUrl: 'http://171.249.156.192:8888/images/'
 };
